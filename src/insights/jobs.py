554c45e821c35e4b744b02d1ccab54f76d00a792
Murilo-MRS <murilosantana1993@gmail.com>
2023-04-27 19:34:25 -0300

    feat; add requisito 2 - unique type

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -13,21 +13,14 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    jobs_list = read(path)
+    job_types = [
+        job_type
+        for job in jobs_list
+        for job_type in job["job_type"].split(",")
+    ]
+    set_types = set(job_types)
+    return list(set_types)
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
