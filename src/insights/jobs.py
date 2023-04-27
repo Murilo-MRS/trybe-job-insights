cdde306bf9214ac781b0f346043dc517faba306d
Murilo-MRS <murilosantana1993@gmail.com>
2023-04-27 19:10:14 -0300

    feat; add requisito 1 - finc reac

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,22 +1,15 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+    with open(path, encoding="utf-8") as file:
+        keys, *jobs = csv.reader(file, delimiter=",", quotechar='"')
+        # zip une 2 listas, dict transforma em dicionario
+        jobs_list = [dict(zip(keys, job)) for job in jobs]
+        return jobs_list
 
 
 def get_unique_job_types(path: str) -> List[str]:
