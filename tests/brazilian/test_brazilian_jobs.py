102559cb3502aaef4e6a6345aef19b2abfa5c5d3
Murilo-MRS <murilosantana1993@gmail.com>
2023-04-28 18:16:59 -0300

    feat: add req 11 - teste de counter

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,17 @@
 # from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    mock_output = [
+        {'salary': '2000', 'title': 'Maquinista', 'type': 'trainee'},
+    ]
+
+    output = read_brazilian_file("tests/mocks/brazilians_jobs.csv")[0]
+    assert "tipo" not in mock_output[0]
+    assert "salario" not in mock_output[0]
+    assert "titulo" not in mock_output[0]
+    assert "title" in mock_output[0]
+    assert "salary" in mock_output[0]
+    assert "type" in mock_output[0]
+    assert output == mock_output[0]
