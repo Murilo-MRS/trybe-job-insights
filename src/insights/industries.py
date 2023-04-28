e1d09a7efa1b1c4f5369fc67de3884e22423a225
Murilo-MRS <murilosantana1993@gmail.com>
2023-04-28 15:13:45 -0300

    feat: add req 7 - filter by industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -18,18 +18,10 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
+    jobs_filtered = [
+        job_filtered
+        for job_filtered in jobs
+        if industry == job_filtered["industry"]
+    ]
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    return jobs_filtered
