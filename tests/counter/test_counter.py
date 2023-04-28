5181229eff918a7fc87da27edc14ec5d6db9d1ad
Murilo-MRS <murilosantana1993@gmail.com>
2023-04-28 18:01:43 -0300

    feat: add req 10 - teste de counter
    ajuda jefferson

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
 # from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("data/jobs.csv", "javascript") == 122
+    assert count_ocurrences("data/jobs.csv", "typescript") == 4
+    assert count_ocurrences("data/jobs.csv", "python") == 1639
+    assert count_ocurrences("data/jobs.csv", "Python") == 1639
